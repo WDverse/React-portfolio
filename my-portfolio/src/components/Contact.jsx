e9a9f72ba27b8386b6405e10dbc3a7be2b04511f
Emmanuel Appiagyei <e_appiagyei@outlook.com>
2023-09-25 05:03:35 -0400

    added about me, contact and resume

---
+++ my-portfolio/src/components/Contact.jsx
@@ -0,0 +1,24 @@
+import Form from 'react-bootstrap/Form';
+
+function Contact() {
+    return (
+        <>
+            <Form name="contact">
+                <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                    <Form.Label>Name:</Form.Label>
+                    <Form.Control type="text" placeholder="Enter your name" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                    <Form.Label>Email address:</Form.Label>
+                    <Form.Control type="email" placeholder="Email Address" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                    <Form.Label>Message:</Form.Label>
+                    <Form.Control as="textarea" placeholder="Leave your message" rows={3} />
+                </Form.Group>
+            </Form>
+        </>
+    )
+};
+
+export default Contact();
---
+++ my-portfolio/src/components/Resume.jsx
@@ -0,0 +1,25 @@
+
+function Resume() {
+    return (
+        <>
+
+            <div name="resume">
+                <h2>Resume</h2>
+                <p>Download my <a href="https://drive.google.com/file/d/1SzImUxrvGugYeV063b9DL5cFN82B-f6N/view?usp=share_link">resume</a></p>
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+
+        </>
+    )
+}
+
+
+export default Resume;
