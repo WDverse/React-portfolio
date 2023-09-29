156e514f73edadad81f30012bdbff4d20237e4ea
Emmanuel Appiagyei <e_appiagyei@outlook.com>
2023-09-29 01:40:59 -0400

    fixed nested structure in aboutme, fixed export in contact.jsx

--- my-portfolio/src/components/Contact.jsx
+++ my-portfolio/src/components/Contact.jsx
@@ -6,19 +6,19 @@ function Contact() {
             <Form name="contact">
                 <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
                     <Form.Label>Name:</Form.Label>
-                    <Form.Control type="text" placeholder="Enter your name" />
+                    <Form.Control type="text" />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
                     <Form.Label>Email address:</Form.Label>
-                    <Form.Control type="email" placeholder="Email Address" />
+                    <Form.Control type="email" />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
                     <Form.Label>Message:</Form.Label>
-                    <Form.Control as="textarea" placeholder="Leave a message" rows={3} />
+                    <Form.Control as="textarea" rows={3} />
                 </Form.Group>
             </Form>
         </>
     )
 };
 
-export default Contact();
+export default Contact;
--- my-portfolio/src/components/Resume.jsx
+++ my-portfolio/src/components/Resume.jsx
@@ -5,7 +5,7 @@ function Resume() {
 
             <div name="resume">
                 <h2>Resume</h2>
-                <p>Download my <a href="https://drive.google.com/file/d/1SzImUxrvGugYeV063b9DL5cFN82B-f6N/view?usp=share_link">resume</a></p>
+                <p>Download my <a href="https://drive.google.com/file/d/1SzImUxrvGugYeV063b9DL5cFN82B-f6N/view?usp=share_link" download="">resume</a></p>
                 <ul>
                     <li></li>
                     <li></li>
