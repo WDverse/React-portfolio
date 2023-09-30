f7535283856ac8272fe02bdddda4f137271d8b5f
Emmanuel Appiagyei <e_appiagyei@outlook.com>
2023-09-29 21:04:26 -0400

    changed project cards

--- my-portfolio/src/components/Project.jsx
+++ my-portfolio/src/components/Project.jsx
@@ -1,27 +1,93 @@
 import Card from 'react-bootstrap/Card';
-import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 
 function Project(props) {
   return (
-    <Row xs={1} md={2} className="g-4">
-      <h2 style={{ width: 1000 }}>{props.title}</h2>
-      {Array.from({ length: 6 }).map((_, idx) => (
-        <Col key={idx}>
-          <Card>
-            {/* <Card.Img variant="top" src="https://www.celoxis.com/cassets/img/pmc/project-management.png" /> */}
+    <>
+      <Row xs={1} md={3} className="g-4">
+        <Col>
+          <Card style={{ width: '25rem' }}>
+            <Card.Img variant="top" src="holder.js/100px160" />
             <Card.Body>
               <Card.Title>Card title</Card.Title>
               <Card.Text>
-                This is a description of the project
+                This is a longer card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
               </Card.Text>
-              <Button href="#">Live Link</Button> <Button type="submit">GitHub Repo</Button>{' '}
             </Card.Body>
           </Card>
         </Col>
-      ))}
-    </Row>
+        <Col>
+        <Card style={{ width: '25rem' }}>
+            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Body>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This is a longer card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col>
+        <Card style={{ width: '25rem' }}>
+            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Body>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This is a longer card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col>
+        <Card style={{ width: '25rem' }}>
+            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Body>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This is a longer card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col>
+        <Card style={{ width: '25rem' }}>
+            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Body>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This is a longer card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col>
+        <Card style={{ width: '25rem' }}>
+            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Body>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This is a longer card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+
+      </Row>
+    </>
   );
 }
 
