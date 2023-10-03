46490d6e173784b3637afa7f8e35d8e7d45852e2
Emmanuel Appiagyei <e_appiagyei@outlook.com>
2023-10-02 22:21:36 -0400

    changed header name

--- my-portfolio/src/components/Header.jsx
+++ my-portfolio/src/components/Header.jsx
@@ -6,7 +6,7 @@ function Header(props) {
         <Card >
             <Card.Header style={{backgroundColor:"#087F8C"}}>
                 <Card.Body className='header-size' >
-                <h1 className='header'>Emmanuel Appiagyei DEBUG</h1>
+                <h1 className='header'>Emmanuel Appiagyei</h1>
                 {props.children}
                 </Card.Body>
             </Card.Header>
