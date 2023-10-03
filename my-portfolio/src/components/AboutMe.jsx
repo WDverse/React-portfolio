686b7df9abf4aa16fa6f5a92aa7c731b2a85eeb0
Emmanuel Appiagyei <e_appiagyei@outlook.com>
2023-10-03 02:18:21 -0400

    added typewriter effect, added reference to reademe

--- my-portfolio/src/components/AboutMe.jsx
+++ my-portfolio/src/components/AboutMe.jsx
@@ -11,6 +11,9 @@ function AboutMe(props) {
                 <h2 className='about-me'>About Me</h2>
                 <Container>
                     <Row>
+                    <div className="typewriter">
+                            <h1>I craft digital worlds, one code at a time.</h1>
+                        </div>
                         <Col xs={6} md={4}>
                             <Image src="https://wdverse.github.io/My-Portfolio/assets/images/emmanuel-appiagyei.jpeg" rounded width={180} />
                         </Col>
@@ -21,6 +24,7 @@ function AboutMe(props) {
                         Hello there! I'm a passionate full stack web developer with a relentless curiosity for all things tech. Currently pursuing a Master of Applied Science degree in Technology Innovation Management at Carleton University, I thrive on creating seamless digital experiences and exploring innovative solutions.
                         <div>With a strong foundation in both front-end and back-end development, I'm dedicated to pushing the boundaries of what's possible in the digital realm.</div>
                         <div>Let's build the future together, one line of code at a time.</div>
+                        
                     </div>
                 </div>
             </div>
